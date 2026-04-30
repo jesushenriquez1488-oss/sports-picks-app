d2296b0eaedee38c629cf1c4511eb9d8cfcc6259
jesushenriquez1488-oss <jesushenriquez1488@gmail.com>
2026-04-30 11:04:21 -0500

    script.js

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+const SUPABASE_URL = "https://chwufitqbxqjbhdixdwk.supabase.co";
+const SUPABASE_KEY = "chwuftiqbxqjbhdixdwk";
+
+const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 const ODDS_API_KEY = "bce7c087b72953e95406c9215e6d24e7";
 const BALLDONTLIE_API_KEY = "a36cf94f-2589-4f0d-a632-a55b1133fe92";
 
