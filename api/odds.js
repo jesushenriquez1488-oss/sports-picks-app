7b438cc233d378bdda4e3b04a089e41bcf92186f
jesushenriquez1488-oss <jesushenriquez1488@gmail.com>
2026-05-06 11:29:04 -0500

    odds.js

--- api/odds.js
+++ api/odds.js
@@ -4,6 +4,13 @@ global.__ODDS_CACHE__ = oddsCache;
 const ODDS_CACHE_TIME = 10 * 60 * 1000;
 
 module.exports = async function handler(req, res) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
+if (req.method === "OPTIONS") {
+  return res.status(200).end();
+}
   if (req.method !== "GET") {
     return res.status(405).json({ error: "Method not allowed" });
   }
