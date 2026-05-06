ac1fa862f5e1403c0841eb50aa4eabfff8be2665
jesushenriquez1488-oss <jesushenriquez1488@gmail.com>
2026-05-06 11:35:58 -0500

    nba-data.js

--- api/nba-data.js
+++ api/nba-data.js
@@ -6,6 +6,13 @@ const TIMEOUT_MS = 10000;
 const SEASON = 2025;
 
 module.exports = async function handler(req, res) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
+if (req.method === "OPTIONS") {
+  return res.status(200).end();
+}
   try {
     const { type, teamId } = req.query;
 
