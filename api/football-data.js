b8bf1d4bdac5b006a01940e891c085ef67c3103f
jesushenriquez1488-oss <jesushenriquez1488@gmail.com>
2026-05-06 11:33:39 -0500

    football-data.js

--- api/football-data.js
+++ api/football-data.js
@@ -596,6 +596,13 @@ function sanitizePicksForPublic(picks, isPremiumUser) {
 }
 
 module.exports = async function handler(req, res) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
+if (req.method === "OPTIONS") {
+  return res.status(200).end();
+}
   try {
     const { type = "nfl", teamA, teamB, season } = req.query;
 
