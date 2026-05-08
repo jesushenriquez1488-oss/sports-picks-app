472be7d1949af6c513735f5d03c7f402d5b1acc9
jesushenriquez1488-oss <jesushenriquez1488@gmail.com>
2026-05-08 11:28:40 -0500

    script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
 
 
-const IS_ADMIN = false; 
+const IS_ADMIN = false;
 
 const MONTHLY_PRICE = 19.99;
 const SINGLE_PICK_PRICE = 1.99;
