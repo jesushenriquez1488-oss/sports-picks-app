7d36f5e0cfc671bd424eb658840f9dcc4b134261
jesushenriquez1488-oss <jesushenriquez1488@gmail.com>
2026-06-09 13:07:21 -0500

    injuries.js

--- api/injuries.js
+++ api/injuries.js
@@ -8,6 +8,40 @@ if (req.method === "OPTIONS") {
 }
   try {
     const { team } = req.query;
+    const TEAM_KEYS = {
+  "Atlanta Hawks": "ATL",
+  "Boston Celtics": "BOS",
+  "Brooklyn Nets": "BKN",
+  "Charlotte Hornets": "CHA",
+  "Chicago Bulls": "CHI",
+  "Cleveland Cavaliers": "CLE",
+  "Dallas Mavericks": "DAL",
+  "Denver Nuggets": "DEN",
+  "Detroit Pistons": "DET",
+  "Golden State Warriors": "GS",
+  "Houston Rockets": "HOU",
+  "Indiana Pacers": "IND",
+  "Los Angeles Clippers": "LAC",
+  "Los Angeles Lakers": "LAL",
+  "Memphis Grizzlies": "MEM",
+  "Miami Heat": "MIA",
+  "Milwaukee Bucks": "MIL",
+  "Minnesota Timberwolves": "MIN",
+  "New Orleans Pelicans": "NO",
+  "New York Knicks": "NY",
+  "Oklahoma City Thunder": "OKC",
+  "Orlando Magic": "ORL",
+  "Philadelphia 76ers": "PHI",
+  "Phoenix Suns": "PHO",
+  "Portland Trail Blazers": "POR",
+  "Sacramento Kings": "SAC",
+  "San Antonio Spurs": "SA",
+  "Toronto Raptors": "TOR",
+  "Utah Jazz": "UTA",
+  "Washington Wizards": "WAS"
+};
+
+const teamKey = TEAM_KEYS[team] || team;
 
     if (!team) {
       return res.status(400).json({ error: "Missing team parameter" });
