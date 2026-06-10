06f402bc977f9764f45ab9493f5b85375f58d162
jesushenriquez1488-oss <jesushenriquez1488@gmail.com>
2026-06-10 15:07:50 -0500

    nba-data.js

--- api/nba-data.js
+++ api/nba-data.js
@@ -6,7 +6,7 @@ const TIMEOUT_MS = 10000;
 const SEASON = 2025;
 
 module.exports = async function handler(req, res) {
- res.setHeader("Access-Control-Allow-Origin", "https://cashedgeapp.com");
+res.setHeader("Access-Control-Allow-Origin", "*");
 res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
 res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
 
