40e6ccf292ca121e7e0270f990d53ae2647c1789
jesushenriquez1488-oss <jesushenriquez1488@gmail.com>
2026-06-10 13:53:18 -0500

    football-data.js

--- api/football-data.js
+++ api/football-data.js
@@ -719,7 +719,7 @@ side: shouldHide ? "Suscríbete para desbloquear" : pick.side || null
 }
 
 module.exports = async function handler(req, res) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Origin", "https://cashedgeapp.com");
 res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
 res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
 
