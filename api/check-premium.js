b6628a45b0cfef956077bf43be94b80008fffe2d
jesushenriquez1488-oss <jesushenriquez1488@gmail.com>
2026-06-10 15:05:57 -0500

    check-premium.js

--- api/check-premium.js
+++ api/check-premium.js
@@ -11,7 +11,7 @@ const supabaseAuth = createClient(
 );
 
 module.exports = async function handler(req, res) {
-  res.setHeader("Access-Control-Allow-Origin", "https://cashedgeapp.com");
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
 
