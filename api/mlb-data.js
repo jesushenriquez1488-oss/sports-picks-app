e0bd7ba89754b4d968aa80395a3c6bf425691cff
jesushenriquez1488-oss <jesushenriquez1488@gmail.com>
2026-06-10 15:07:13 -0500

    mlb-data.js

--- api/mlb-data.js
+++ api/mlb-data.js
@@ -68,7 +68,7 @@ const STADIUM_COORDS = {
   "Las Vegas Ballpark": { lat: 36.1597, lon: -115.3200 },
 };
 export default async function handler(req, res) {
- res.setHeader("Access-Control-Allow-Origin", "https://cashedgeapp.com");
+res.setHeader("Access-Control-Allow-Origin", "*");
 res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
 res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
 
