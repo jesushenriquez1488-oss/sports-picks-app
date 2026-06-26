66bd69e1b31e5f6ae0a587b225b00c830d680274
jesushenriquez1488-oss <jesushenriquez1488@gmail.com>
2026-06-26 11:48:13 -0500

    sports.js

---
+++ bot/config/sports.js
@@ -0,0 +1,9 @@
+export const ACTIVE_SPORTS = [
+  "baseball_mlb",
+  "basketball_wnba",
+  "basketball_nba",
+  "americanfootball_nfl",
+  "americanfootball_ncaaf",
+  "basketball_ncaab",
+  "icehockey_nhl"
+];
