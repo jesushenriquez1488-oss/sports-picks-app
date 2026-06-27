c0cdb7ce8ea8ac344567378d880c0bde8eb33631
jesushenriquez1488-oss <jesushenriquez1488@gmail.com>
2026-06-27 14:56:34 -0500

    script.js

--- script.js
+++ script.js
@@ -1422,7 +1422,7 @@ if (window.currentSport === "wnba") {
           <div id="result${index}"></div>
         </div>
       `;
-    }
+    });
   } catch (error) {
     status.innerHTML = "Error: " + error.message;
   }
