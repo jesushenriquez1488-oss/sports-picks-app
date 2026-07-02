108b02fe4764bab0c0c58a39ea1028596d593bd4
jesushenriquez1488-oss <jesushenriquez1488@gmail.com>
2026-07-01 22:57:27 -0500

    mlb-data.js

--- api/mlb-data.js
+++ api/mlb-data.js
@@ -67,7 +67,7 @@ const STADIUM_COORDS = {
   "Petco Park": { lat: 32.7073, lon: -117.1573 },
   "Las Vegas Ballpark": { lat: 36.1597, lon: -115.3200 },
 };
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
 res.setHeader("Access-Control-Allow-Origin", "*");
 res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
 res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
