086845b6f079bfa2465edba66399eec3abc0f26e
jesushenriquez1488-oss <jesushenriquez1488@gmail.com>
2026-07-17 14:28:27 -0500

    football-data.js

--- api/football-data.js
+++ api/football-data.js
@@ -2131,8 +2131,9 @@ try {
     // ===== FIN CACHE =====
     const selectedSeason = Number(season) || getDefaultSeason();
 
-   const teamARef = normalizeTeam(teamA, type);
-    const teamBRef = normalizeTeam(teamB, type);
+   const teamARef = normalizeTeam(teamA);
+    const teamBRef = normalizeTeam(teamB);
+    
     const allGames = await getSeasonGames(type, selectedSeason);
 
     const teamAGames = buildTeamGamesFromSeason(allGames, teamARef);
