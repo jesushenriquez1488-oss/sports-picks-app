189ff20d6105a59f155fb320a42290c2c52a0dfc
jesushenriquez1488-oss <jesushenriquez1488@gmail.com>
2026-08-12 16:48:05 -0500

    featured-premium-pick.js

--- api/featured-premium-pick.js
+++ api/featured-premium-pick.js
@@ -15,7 +15,15 @@ function getChicagoDate() {
 }
 
 module.exports = async function handler(req, res) {
+  
   try {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+res.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS");
+res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+if (req.method === "OPTIONS") {
+  return res.status(204).end();
+}
     if (req.method !== "GET") {
       return res.status(405).json({
         error: "Method not allowed"
