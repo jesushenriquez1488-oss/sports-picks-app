4f1050d5396236b957ed57141b3e6559db7fd198
jesushenriquez1488-oss <jesushenriquez1488@gmail.com>
2026-09-20 08:57:23 -0500

    ingest-quote.js

--- api/market-intelligence/ingest-quote.js
+++ api/market-intelligence/ingest-quote.js
@@ -11,7 +11,7 @@ const {
   require("../../lib/marketPipeline");
 const {
   buildCanonicalMarketQuote,
-  validateCanonicalMarketQuote
+  validateCanonicalMarketQuote 
 } =
   require("./marketProviderAdapter");
 
